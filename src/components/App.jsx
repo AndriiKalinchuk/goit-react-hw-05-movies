cf03ec347baba16043c734342c62f1540e93af85
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-06-07 19:33:41 +0300

    structure-home-movie

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import NotFoundPage from 'pages/NotFoundPages/NotFoundPage';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import { SharedLayout } from './Layout/SharedLayout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />}>
+          {/* <Route path=":movieId'" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route> */}
+        </Route>
+      </Route>
+    </Routes>
   );
 };
