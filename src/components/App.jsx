ab62ef76ac1ae620a7672277f4df3559e71f74c0
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-06-09 15:38:55 +0300

    +moviedetails

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NotFoundPage from 'pages/NotFoundPages/NotFoundPage';
 import Home from 'pages/Home';
@@ -11,11 +12,10 @@ export const App = () => {
       <Route path="/" element={<SharedLayout />}>
         <Route path="*" element={<NotFoundPage />} />
         <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />}>
-          <Route path=":movieId'" element={<MovieDetails />}>
-            {/* <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} /> */}
-          </Route>
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId/*" element={<MovieDetails />}>
+          {/* <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} /> */}
         </Route>
       </Route>
     </Routes>
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,116 +1,82 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
-
-import {
-  BackdropContainer,
-  BackdropPoster,
-  Genre,
-  Genres,
-  GradientBlockBottom,
-  GradientBlockTop,
-  MainTitle,
-  NavLink,
-  WrapperDetails,
-  WrapperMovie,
-} from './MovieDetails.styled';
-
 import { getMoviesDetailsById } from 'service/movies-service';
 
 const MovieDetails = () => {
-  const [movieDetails, setMovieDetails] = useState([]);
-  const [error, setError] = useState(false);
+  const [movieDetails, setMovieDetails] = useState(null);
 
   const location = useLocation();
-  //   const backLinkLocationRef = useRef(location.state?.from ?? '/');
-
   const { movieId } = useParams();
 
   useEffect(() => {
-    (async () => {
-      try {
-        setError(false);
-        const data = await getMoviesDetailsById(movieId);
-
-        setMovieDetails(data.results);
-      } catch (error) {
-        alert(error.message);
-      }
-    })();
+    const getDetails = async () => {
+      const data = await getMoviesDetailsById(movieId);
+      setMovieDetails(data);
+    };
+    getDetails();
   }, [movieId]);
 
-  if (!movieId) {
-    return;
+  if (!movieDetails) {
+    return <div>Loading...</div>;
   }
 
-  const { poster_path, original_title, title, release_date, genres, overview } =
-    movieDetails;
+  const {
+    poster_path,
+    original_title,
+    title,
+    vote_average,
+    release_date,
+    genres,
+    overview,
+  } = movieDetails;
 
   return (
     <>
-      {!error && (
+      <Link to={location.state?.from ?? '/'}>Go back</Link>
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/original${poster_path}`
+            : 'https://www.technocrazed.com/wp-content/uploads/2013/06/Discover_how_cinema_posters_similar_back-443x1024.jpg'
+        }
+        alt={title || original_title}
+        width="1200"
+      />
+      <h1>
+        {title || original_title}
+        {release_date && <span> ({parseInt(release_date)})</span>}
+      </h1>
+
+      {vote_average && (
         <>
-          {/* фонове зображення */}
-          {poster_path && (
-            <>
-              <BackdropContainer>
-                <GradientBlockTop></GradientBlockTop>
-                {`https://image.tmdb.org/t/p/original${poster_path}` && (
-                  <BackdropPoster>
-                    <img
-                      src={
-                        poster_path
-                          ? `https://image.tmdb.org/t/p/original${poster_path}`
-                          : 'https://www.technocrazed.com/wp-content/uploads/2013/06/Discover_how_cinema_posters_similar_back-443x1024.jpg'
-                      }
-                      alt={title || original_title}
-                      width="1200"
-                    />
-                  </BackdropPoster>
-                )}
-                <GradientBlockBottom></GradientBlockBottom>
-              </BackdropContainer>
-            </>
-          )}
-          <WrapperMovie>
-            <WrapperDetails>
-              {/* назва фільму */}
-              <MainTitle>
-                {title || original_title}
-                {release_date && <span> ({parseInt(release_date)})</span>}
-              </MainTitle>
-
-              {/* жанри фільму */}
-              {genres && (
-                <Genres>
-                  {genres.map((genre, index) => (
-                    <Genre key={index}>{genre.name}</Genre>
-                  ))}
-                </Genres>
-              )}
-
-              {/* огляд фільму */}
-              {overview && (
-                <>
-                  <h2>{'moviesPage.overview'}</h2>
-                  <p>{overview}</p>
-                </>
-              )}
-            </WrapperDetails>
-          </WrapperMovie>
-          <Link to={location.state?.from ?? '/'}>Go back</Link>
-          <nav>
-            <NavLink to="cast" state={location.state}>
-              {'cast'}
-            </NavLink>
-            <NavLink to="reviews" state={location.state}>
-              {'reviews'}
-            </NavLink>
-          </nav>
-          <Suspense fallback={<div> {'loading'}</div>}>
-            <Outlet />
-          </Suspense>
+          <p>User Score:{vote_average}</p>
         </>
       )}
+
+      {overview && (
+        <>
+          <h2>Overview</h2>
+          <p>{overview}</p>
+        </>
+      )}
+
+      {genres && (
+        <>
+          <h2>Genres:</h2>
+          <ul>
+            {genres.map((genre, index) => (
+              <li key={index}>{genre.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      <nav>
+        <Link to={`${location.pathname}/cast`}>Cast</Link>
+        <Link to={`${location.pathname}/reviews`}>Reviews</Link>
+      </nav>
+
+      <Outlet />
     </>
   );
 };
